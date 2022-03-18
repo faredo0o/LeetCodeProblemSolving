cd90ca703bfbe4ca7967dfe7c6d3f670ee790451
faredo0o <mastermail@gmail.com>
2022-03-18 14:20:01 +0200

    SwapNodesInPairs

--- src/com/faredo0o/SwapNodesInPairs.java
+++ src/com/faredo0o/SwapNodesInPairs.java
@@ -3,10 +3,22 @@ package com.faredo0o;
 
 public class SwapNodesInPairs {
     public static void main(String[] args) {
+        ListNode head=new ListNode(1,
+                        new ListNode(2,
+                        new ListNode(3,
+                                new ListNode(4))));
+        System.out.println(swapPairs(head));
 
     }
-    public static ListNode swapPairs(ListNode head){
+    public static ListNode swapPairs(ListNode head) {
 
-  return null;
+
+        if(head==null) return null;
+        if(head.next==null) return head;
+        ListNode current=head;
+        ListNode next=head.next;
+        head.next=swapPairs(next.next);
+        next.next=head;
+       return next;
     }
 }
